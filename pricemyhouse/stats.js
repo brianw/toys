5e87b4c2306b55ab6c7a8fe1b600746535268f9f
Brian White <brian@dogg.ie>
2025-11-09 21:50:50 +0000

    stats.js

---
+++ pricemyhouse/stats.js
@@ -0,0 +1,570 @@
+const STATS = {
+  "trainSetSize": 58581,
+  "testR2": 0.8340722322463989,
+  "eircodeToImpact": [
+    {
+      "eircode": "D06",
+      "impact": 449076
+    },
+    {
+      "eircode": "D04",
+      "impact": 434268
+    },
+    {
+      "eircode": "A94",
+      "impact": 418103
+    },
+    {
+      "eircode": "A96",
+      "impact": 386234
+    },
+    {
+      "eircode": "D14",
+      "impact": 372938
+    },
+    {
+      "eircode": "D6W",
+      "impact": 362144
+    },
+    {
+      "eircode": "K36",
+      "impact": 284420
+    },
+    {
+      "eircode": "D03",
+      "impact": 270003
+    },
+    {
+      "eircode": "D16",
+      "impact": 268075
+    },
+    {
+      "eircode": "D18",
+      "impact": 267437
+    },
+    {
+      "eircode": "D02",
+      "impact": 256408
+    },
+    {
+      "eircode": "A63",
+      "impact": 219404
+    },
+    {
+      "eircode": "D13",
+      "impact": 212655
+    },
+    {
+      "eircode": "D09",
+      "impact": 212027
+    },
+    {
+      "eircode": "D05",
+      "impact": 208544
+    },
+    {
+      "eircode": "K34",
+      "impact": 204815
+    },
+    {
+      "eircode": "A98",
+      "impact": 196660
+    },
+    {
+      "eircode": "D08",
+      "impact": 188342
+    },
+    {
+      "eircode": "D07",
+      "impact": 182846
+    },
+    {
+      "eircode": "D12",
+      "impact": 165606
+    },
+    {
+      "eircode": "P17",
+      "impact": 155609
+    },
+    {
+      "eircode": "D15",
+      "impact": 136687
+    },
+    {
+      "eircode": "D20",
+      "impact": 118013
+    },
+    {
+      "eircode": "K78",
+      "impact": 114236
+    },
+    {
+      "eircode": "W23",
+      "impact": 113880
+    },
+    {
+      "eircode": "A41",
+      "impact": 109690
+    },
+    {
+      "eircode": "A86",
+      "impact": 102867
+    },
+    {
+      "eircode": "D11",
+      "impact": 102439
+    },
+    {
+      "eircode": "D01",
+      "impact": 101704
+    },
+    {
+      "eircode": "K67",
+      "impact": 99250
+    },
+    {
+      "eircode": "A45",
+      "impact": 98301
+    },
+    {
+      "eircode": "A42",
+      "impact": 97125
+    },
+    {
+      "eircode": "A67",
+      "impact": 80065
+    },
+    {
+      "eircode": "T12",
+      "impact": 79576
+    },
+    {
+      "eircode": "D24",
+      "impact": 78821
+    },
+    {
+      "eircode": "K45",
+      "impact": 69581
+    },
+    {
+      "eircode": "P81",
+      "impact": 69276
+    },
+    {
+      "eircode": "P31",
+      "impact": 67854
+    },
+    {
+      "eircode": "A85",
+      "impact": 67457
+    },
+    {
+      "eircode": "K56",
+      "impact": 66586
+    },
+    {
+      "eircode": "W91",
+      "impact": 63372
+    },
+    {
+      "eircode": "D22",
+      "impact": 55913
+    },
+    {
+      "eircode": "H71",
+      "impact": 55704
+    },
+    {
+      "eircode": "A84",
+      "impact": 52592
+    },
+    {
+      "eircode": "D17",
+      "impact": 52554
+    },
+    {
+      "eircode": "H91",
+      "impact": 48377
+    },
+    {
+      "eircode": "D10",
+      "impact": 48153
+    },
+    {
+      "eircode": "P43",
+      "impact": 47993
+    },
+    {
+      "eircode": "T45",
+      "impact": 30193
+    },
+    {
+      "eircode": "P85",
+      "impact": 19900
+    },
+    {
+      "eircode": "K32",
+      "impact": 17673
+    },
+    {
+      "eircode": "W12",
+      "impact": 14551
+    },
+    {
+      "eircode": "T23",
+      "impact": 10124
+    },
+    {
+      "eircode": "R56",
+      "impact": 9420
+    },
+    {
+      "eircode": "P24",
+      "impact": 7131
+    },
+    {
+      "eircode": "P25",
+      "impact": 877
+    },
+    {
+      "eircode": "A83",
+      "impact": 0
+    },
+    {
+      "eircode": "C04",
+      "impact": 0
+    },
+    {
+      "eircode": "D25",
+      "impact": 0
+    },
+    {
+      "eircode": "P14",
+      "impact": 0
+    },
+    {
+      "eircode": "R51",
+      "impact": 0
+    },
+    {
+      "eircode": "T34",
+      "impact": 0
+    },
+    {
+      "eircode": "T56",
+      "impact": 0
+    },
+    {
+      "eircode": "C15",
+      "impact": -1187
+    },
+    {
+      "eircode": "X35",
+      "impact": -2135
+    },
+    {
+      "eircode": "W34",
+      "impact": -2628
+    },
+    {
+      "eircode": "P72",
+      "impact": -2879
+    },
+    {
+      "eircode": "Y14",
+      "impact": -7665
+    },
+    {
+      "eircode": "A92",
+      "impact": -7896
+    },
+    {
+      "eircode": "F28",
+      "impact": -8057
+    },
+    {
+      "eircode": "V94",
+      "impact": -8850
+    },
+    {
+      "eircode": "V93",
+      "impact": -10065
+    },
+    {
+      "eircode": "H65",
+      "impact": -10386
+    },
+    {
+      "eircode": "R95",
+      "impact": -11501
+    },
+    {
+      "eircode": "P32",
+      "impact": -11870
+    },
+    {
+      "eircode": "P36",
+      "impact": -22613
+    },
+    {
+      "eircode": "P75",
+      "impact": -24972
+    },
+    {
+      "eircode": "X42",
+      "impact": -29861
+    },
+    {
+      "eircode": "Y25",
+      "impact": -31198
+    },
+    {
+      "eircode": "A91",
+      "impact": -33581
+    },
+    {
+      "eircode": "V23",
+      "impact": -33585
+    },
+    {
+      "eircode": "X91",
+      "impact": -36743
+    },
+    {
+      "eircode": "P47",
+      "impact": -38734
+    },
+    {
+      "eircode": "P61",
+      "impact": -39453
+    },
+    {
+      "eircode": "P12",
+      "impact": -40234
+    },
+    {
+      "eircode": "V95",
+      "impact": -43059
+    },
+    {
+      "eircode": "Y35",
+      "impact": -45095
+    },
+    {
+      "eircode": "N37",
+      "impact": -45443
+    },
+    {
+      "eircode": "N91",
+      "impact": -45821
+    },
+    {
+      "eircode": "R45",
+      "impact": -47083
+    },
+    {
+      "eircode": "V14",
+      "impact": -49661
+    },
+    {
+      "eircode": "H62",
+      "impact": -51763
+    },
+    {
+      "eircode": "F91",
+      "impact": -51825
+    },
+    {
+      "eircode": "R14",
+      "impact": -52957
+    },
+    {
+      "eircode": "R21",
+      "impact": -55077
+    },
+    {
+      "eircode": "P67",
+      "impact": -56515
+    },
+    {
+      "eircode": "R35",
+      "impact": -58076
+    },
+    {
+      "eircode": "E91",
+      "impact": -58862
+    },
+    {
+      "eircode": "R93",
+      "impact": -59488
+    },
+    {
+      "eircode": "E21",
+      "impact": -63156
+    },
+    {
+      "eircode": "R32",
+      "impact": -63975
+    },
+    {
+      "eircode": "V92",
+      "impact": -65203
+    },
+    {
+      "eircode": "E25",
+      "impact": -72666
+    },
+    {
+      "eircode": "E45",
+      "impact": -72939
+    },
+    {
+      "eircode": "F23",
+      "impact": -73190
+    },
+    {
+      "eircode": "Y34",
+      "impact": -73642
+    },
+    {
+      "eircode": "Y21",
+      "impact": -74665
+    },
+    {
+      "eircode": "H54",
+      "impact": -76393
+    },
+    {
+      "eircode": "P51",
+      "impact": -77705
+    },
+    {
+      "eircode": "A82",
+      "impact": -78313
+    },
+    {
+      "eircode": "V42",
+      "impact": -78533
+    },
+    {
+      "eircode": "E32",
+      "impact": -79291
+    },
+    {
+      "eircode": "E41",
+      "impact": -79421
+    },
+    {
+      "eircode": "E53",
+      "impact": -80211
+    },
+    {
+      "eircode": "V15",
+      "impact": -83177
+    },
+    {
+      "eircode": "A81",
+      "impact": -84094
+    },
+    {
+      "eircode": "P56",
+      "impact": -84334
+    },
+    {
+      "eircode": "H16",
+      "impact": -89127
+    },
+    {
+      "eircode": "V35",
+      "impact": -89625
+    },
+    {
+      "eircode": "H18",
+      "impact": -91164
+    },
+    {
+      "eircode": "A75",
+      "impact": -94048
+    },
+    {
+      "eircode": "V31",
+      "impact": -94805
+    },
+    {
+      "eircode": "E34",
+      "impact": -94891
+    },
+    {
+      "eircode": "R42",
+      "impact": -99113
+    },
+    {
+      "eircode": "H12",
+      "impact": -100111
+    },
+    {
+      "eircode": "F92",
+      "impact": -100298
+    },
+    {
+      "eircode": "F26",
+      "impact": -101277
+    },
+    {
+      "eircode": "F42",
+      "impact": -101444
+    },
+    {
+      "eircode": "F31",
+      "impact": -105261
+    },
+    {
+      "eircode": "F56",
+      "impact": -106468
+    },
+    {
+      "eircode": "H53",
+      "impact": -109105
+    },
+    {
+      "eircode": "F94",
+      "impact": -112228
+    },
+    {
+      "eircode": "F12",
+      "impact": -113048
+    },
+    {
+      "eircode": "H23",
+      "impact": -113769
+    },
+    {
+      "eircode": "N41",
+      "impact": -119297
+    },
+    {
+      "eircode": "F52",
+      "impact": -121926
+    },
+    {
+      "eircode": "N39",
+      "impact": -122476
+    },
+    {
+      "eircode": "H14",
+      "impact": -137351
+    },
+    {
+      "eircode": "F45",
+      "impact": -140426
+    },
+    {
+      "eircode": "F35",
+      "impact": -148475
+    },
+    {
+      "eircode": "F93",
+      "impact": -149746
+    }
+  ]
+};
